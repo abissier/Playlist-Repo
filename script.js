498541b555e7b8ea1f7040bdb9838ef53d92176e
Angelee Shaw <angeleefshaw@gmail.com>
2020-12-01 21:19:17 -0500

    Fixes

--- script.js
+++ script.js
@@ -32,10 +32,6 @@ $.ajax({
 //get users 3 favorite artists
 
 //returns track details from artists 	searchtrack.php?s={Artist_Name}&t={Single_Name}
-const artistOneUrl = 'theaudiodb.com/api/v1/json/1/searchalbum.php?s=' + firstArtist
-const artistTwoUrl = 'theaudiodb.com/api/v1/json/1/searchalbum.php?s=' + secondArtist
-const artistThreeUrl ='theaudiodb.com/api/v1/json/1/searchalbum.php?s=' + thirdArtist
-        
 //get artist #1
 const firstArtist = $(`#artist-1`).val().trim()
 //get artist #2
@@ -44,12 +40,18 @@ const secondArtist = $(`#artist-2`).val().trim()
 const thirdArtist = $(`#artist-3`).val().trim()
 //array to hold all songs
 const playlist = []
+
+const artistOneUrl = 'theaudiodb.com/api/v1/json/1/searchalbum.php?s=' + firstArtist
+const artistTwoUrl = 'theaudiodb.com/api/v1/json/1/searchalbum.php?s=' + secondArtist
+const artistThreeUrl ='theaudiodb.com/api/v1/json/1/searchalbum.php?s=' + thirdArtist
+        
+
             
 function submitInputs() {
   const settings = {
     "async": true,
     "crossDomain": true,
-    "url": "https://theaudiodb.p.rapidapi.com/searchtrack.php?t=trackname&s=artist",
+    "url": "https://theaudiodb.p.rapidapi.com/searchtrack.php?t=yellow&s=coldplay",
     "method": "GET",
     "headers": {
       "x-rapidapi-key": "1eaefda46fmsh7f49637a27e8f45p19f2d6jsn2ebdbc6ee733",
@@ -63,13 +65,13 @@ function submitInputs() {
 
 }
         
-$(`#btnID`).on(click, function(e) {
+$(`#btn`).on('click', function(e) {
   e.preventDefault()
   submitInputs()
   //get 3 songs fronm each playlist
   //add these three to an array
-  playlist.push()
-  $(`#Id`).innerHTML = playlist
+  //playlist.push()
+  //$(`#Id`).innerHTML = playlist
                 
 })
         
