7309697f72337dcdd4f29e291ff11c36b8901ca8
Heath Ward <heathw24@outlook.com>
2020-12-01 20:10:57 -0500

    created variables for MusixMatch API

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+//============ MusixMatch Info =================
+
+var songID;
+var MusixMatchKey = "f87914fabf3b652d6e3500c66b6259d6";
+var MusixMatchURL = "https://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=" + MusixMatchKey + "&track_id=" + songID ;
+
+$.ajax({
+    url: MusixMatchURL,
+    method: "GET"
+}).then(function(LyricsResponse){
+    console.log(LyricsResponse);
+
+
+});
+
+//=======================================================================
