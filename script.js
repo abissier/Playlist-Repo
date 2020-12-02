2a570d588b70e7a366214b8161bab283a1b5be03
abissier <71886314+abissier@users.noreply.github.com>
2020-12-01 20:47:30 -0800

    added ids to html, added @media-screen

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 //============ MusixMatch Info =================
 
-var songID;
+var songID = "15753433";
 var MusixMatchKey = "f87914fabf3b652d6e3500c66b6259d6";
 var MusixMatchURL = "https://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=" + MusixMatchKey + "&track_id=" + songID ;
 
